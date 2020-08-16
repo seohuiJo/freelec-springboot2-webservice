1808889ffb2c008bd69160e018b6314bf03f1958
seohuiJo <41426853+seohuiJo@users.noreply.github.com>
2020-08-16 12:20:37 +0900

    JpaConfig 추가

--- 소스코드/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
+++ 소스코드/freelec-springboot2-webservice/src/main/java/com/jojoldu/book/springboot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing  // JPA Auditing 활성화
+/*@EnableJpaAuditing  // JPA Auditing 활성화
 // @SpringBootApplication
 // 스프링부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정함
-// @SpringBootApplication이 있는 위치부터 설정을 읽기 때문에, 이 클래스는 항상 프로젝트의 최상단에 위치해야 함
+// @SpringBootApplication이 있는 위치부터 설정을 읽기 때문에, 이 클래스는 항상 프로젝트의 최상단에 위치해야 함*/
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
